25044120263f41d6b7bb6a074debb017d0f822da
Konstantinos Kallas <konstantinos.kallas@hotmail.com>
2021-06-21 12:40:48 -0400

    Successfully make a request to the text service

--- socialNetwork/src/ComposePostService/MyComposePostService.py
+++ socialNetwork/src/ComposePostService/MyComposePostService.py
@@ -4,7 +4,7 @@ sys.path.append('gen-py')
 # print("hi", glob.glob('../../thrift-0.14.1/lib/py/build/lib*'))
 # sys.path.insert(0, glob.glob('../../thrift-0.14.1/lib/py/build/lib*')[0])
 
-from social_network import ComposePostService
+from social_network import ComposePostService, TextService
 # from ComposePostService import *
 
 from thrift.transport import TSocket
@@ -58,8 +58,9 @@ def set_up_tracer(config_file_path, service):
         exit(1)
 
 class ComposePostHandler:
-    def __init__(self):
+    def __init__(self, text_service_client):
         self.log = {}
+        self.text_service_client = text_service_client
 
     ## TODO: Regenerate the thrift files so that carrier is there
     def ComposePost(self, req_id, username, user_id,    
@@ -74,15 +75,64 @@ class ComposePostHandler:
         # log("ParentContext:", parent_span_context)
         with tracer.start_span(operation_name='compose_post_server', 
                                child_of=parent_span_context) as span:
-            ## TODO: Remove. This is just here for debugging.
-            span.log_kv({'event': 'test message', 'life': 42})
+            ## This was here just for debugging.
+            # span.log_kv({'event': 'test message', 'life': 42})
+
+            ## TODO: I am not sure what I need to do with this
+            writer_text_map = {}
 
             ## TODO: Make a call to another service properly.
+            self._ComposeTextHelper(req_id, text, span, writer_text_map)
         return
+
+    def _ComposeTextHelper(self, req_id, text, span, carrier):
+        client = self.text_service_client
+
+        ## TODO: Do correct logging, tracing!
+
+        ## TODO: Create a new span here.
+        opentracing.global_tracer().inject(
+            span_context=span.context,
+            format=Format.TEXT_MAP,
+            carrier=carrier
+        )
+
+        ## TODO: Do we maybe need to connect to the transport here?
+
+        ## TODO: What kind of errors to we need to catch here?
+        return_text = client.ComposeText(req_id, text, carrier)
+
+        return return_text
+
+
+
 
 
 ## TODO: Before making the tracer work we can do with logging, no problem.
 
+
+def TextServiceClient():
+    ## TODO: Get those from the config file
+    text_service_port = 9090
+    text_service_addr = "text-service"
+    ## Setup the socket
+    transport = TSocket.TSocket(host=text_service_addr,
+                                port=text_service_port)
+    ## Configure the transport layer to correspond to the expected transport
+    transport = TTransport.TFramedTransport(transport)
+
+    ## Configure the protocol to be binary as expected from the service
+    protocol = TBinaryProtocol.TBinaryProtocol(transport)
+
+    ## Create the client
+    client = TextService.Client(protocol)
+
+    ## Connect to the client
+    ## TODO: Understand what this does
+    ## TODO: Where do we need to catch errors?
+    transport.open()
+
+    return client
 
 if __name__ == '__main__':
     ## TODO: Sigint handler
@@ -92,11 +142,13 @@ if __name__ == '__main__':
     set_up_tracer("TODO", 'compose-post-service')
 
     ## TODO: Load the config file
-    port = 9090
+    service_port = 9090
 
-    handler = ComposePostHandler()
+    text_service_client = TextServiceClient()
+
+    handler = ComposePostHandler(text_service_client)
     processor = ComposePostService.Processor(handler)
-    transport = TSocket.TServerSocket(host='0.0.0.0', port=port)
+    transport = TSocket.TServerSocket(host='0.0.0.0', port=service_port)
     tfactory = TTransport.TFramedTransportFactory()
     pfactory = TBinaryProtocol.TBinaryProtocolFactory()
 
